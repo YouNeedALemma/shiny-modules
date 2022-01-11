7e894df72b5318b2be34947d15f00e38a0ed9141
Oliver Alexander <oliver.alexander96@outlook.com>
2022-01-11 17:48:59 +1100

    forgot to save devfile

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ## 
 golem::fill_desc(
   pkg_name = "modules", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  pkg_title = "shiny_modules", # The Title of the package containing the App 
+  pkg_description = "Testing out shiny modules", # The Description of the package containing the App 
+  author_first_name = "Oliver", # Your First Name
+  author_last_name = "Alexander", # Your Last Name
+  author_email = "oliver.alexander96@outlook.com", # Your Email
+  repo_url = "https://github.com/YouNeedALemma/shiny-modules" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -16,11 +16,12 @@
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
 usethis::use_package( "thinkr" )
-
+usethis::use_package( "ggplot2" )
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module( name = "name_of_module1" ) # Name of the module
-golem::add_module( name = "name_of_module2" ) # Name of the module
+golem::add_module( name = "first" ) # Name of the module
+golem::add_module( name = "plot" ) # Name of the module
+golem::add_module( name = "table")
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
